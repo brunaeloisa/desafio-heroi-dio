386ad4bf08663558e5b9282a46753647a0cadc68
bruna <brunaeloisa7@gmail.com>
2024-11-09 18:35:30 -0300

    solução com javascript

---
+++ scripts/classificador.js
@@ -0,0 +1,25 @@
+const nome = "Zeus";
+const xp = 9500;
+let nivel;
+
+// Calcula nível do herói
+if (xp <= 1000) {
+  nivel = "Ferro";
+} else if (xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp <= 5000) {
+  nivel = "Prata";
+} else if (xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp <= 8000) {
+  nivel = "Platina";
+} else if (xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+// Exibe mensagem ao usuário
+console.log(`O herói de nome ${nome} está no nível ${nivel}`);
